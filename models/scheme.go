4f2ebc44c04726a1507645af174b43827fcbc38c
ryanlimdx <ryanlimdx@gmail.com>
2024-09-05 19:36:27 +0800

    Add models for the entities

--- models/scheme.go
+++ models/scheme.go
@@ -1,2 +1,22 @@
 // Contains the structure of the entities involved.
 package models
+
+type Scheme struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Criteria []Criteria `json:"criteria"`
+	Benefits []Benefit `json:"benefits"`
+}
+
+type Criteria struct {
+	ID	string `json:"id"`
+	CriteriaLevel string `json:"criteria_level"`
+	CriteriaType string `json:"criteria_type"`
+	Status string `json:"status"`
+}
+
+type Benefit struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+	Amount float64 `json:"amount"`
+}
